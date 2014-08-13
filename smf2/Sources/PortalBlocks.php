b841dda19a7d32004c042b02162ab73a60fa6436
Selman Eser <basmimarsinan@hotmail.com>
2014-08-13 22:30:50 +0200

    ! Duplicate language string index caused a capitalization mistake.

--- smf2/Sources/PortalBlocks.php
+++ smf2/Sources/PortalBlocks.php
@@ -310,6 +310,8 @@ function sp_whosOnline($parameters, $id, $return_parameters = false)
 
 	$online_today = !empty($parameters['online_today']);
 
+	loadLanguage('index', '', false, true);
+
 	$stats = ssi_whosOnline('array');
 
 	echo '
