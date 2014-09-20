703c5c4de121be8149ba3a35ddc2e7a2a5b62894
Selman Eser <selman.eser@bosnasema.ba>
2014-09-20 15:11:03 +0200

    ! Changed the license.

--- smf1/Themes/default/PortalAdminArticles.template.php
+++ smf1/Themes/default/PortalAdminArticles.template.php
@@ -1,5 +1,14 @@
 <?php
-// Version: 2.3.6; PortalAdminArticles
+
+/**
+ * @package SimplePortal
+ *
+ * @author SimplePortal Team
+ * @copyright 2014 SimplePortal Team
+ * @license BSD 3-clause
+ *
+ * @version 2.3.6
+ */
 
 function template_article_list()
 {
